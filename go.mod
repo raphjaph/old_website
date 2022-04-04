c5011c0bcb3ce59e8b695e34b381e5b14bb72330
raphjaph <raphjaph@protonmail.com>
2022-04-04 20:52:06 +0200

    added custom webserver to have an authenticated path

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/raphjaph/website
+
+go 1.17
+
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	github.com/gorilla/mux v1.8.0
+)
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	docopt "github.com/docopt/docopt-go"
+	"github.com/gorilla/mux"
+)
+
+// TODO: get these from config or environment variable
+const (
+	secret    = "JzT+pAqbdYMJAWZonC5Z"
+	address   = "127.0.0.1:3333"
+	staticDir = "/Users/raphael"
+	authPath  = "/s/books/"
+)
+
+const USAGE = `website
+
+Usage:
+  website run 
+  website new-user <username>
+`
+
+var router = mux.NewRouter()
+
+func main() {
+	opts, err := docopt.ParseDoc(USAGE)
+	if err != nil {
+		return
+	}
+
+	switch {
+	case opts["run"].(bool):
+		runServer()
+	case opts["new-user"].(bool):
+		createUser(opts)
+	}
+
+	return
+}
+
+func runServer() {
+	// all requests flow through authentication check and logger
+	router.Use(authAndLogMiddleware)
+
+	router.PathPrefix("/s/").Handler(http.StripPrefix("/s/", http.FileServer(http.Dir(staticDir))))
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         address,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Printf("Listening on %v...\n", address)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func authAndLogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		// TODO: rate limit
+		// only authenticate path to the actual book files
+		if strings.HasPrefix(req.URL.Path, authPath) {
+			username, password, ok := req.BasicAuth()
+			if ok {
+				expectedPassword := computePassword(username)
+
+				// was reading about side channel attacks so I overengineered this password check
+				if subtle.ConstantTimeCompare([]byte(password), []byte(expectedPassword)) == 1 {
+					log.Printf("successfully authenticated %v", username)
+					next.ServeHTTP(w, req)
+					return
+				}
+
+				log.Printf("failed login attempt from %v for resource %v\n", req.RemoteAddr, req.RequestURI)
+				w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+
+			}
+			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		} else {
+			// TODO: some logging?
+			log.Println(req.URL)
+		}
+
+		// serve next if not trying to access /books/
+		next.ServeHTTP(w, req)
+		return
+	})
+}
+
+// only creates a sort unique key (password) for the user; nothing stored in a database
+func createUser(opts docopt.Opts) {
+	username, ok := opts["<username>"].(string)
+	if !ok {
+		fmt.Println("error parsing commandline argument")
+		return
+	}
+
+	fmt.Println(computePassword(username))
+}
+
+// computes a password for a username
+// password = hash(username || secret); secret set in env or config
+func computePassword(name string) string {
+	name = strings.ToLower(name)
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write([]byte(name))
+	return hex.EncodeToString(mac.Sum(nil))
+}
